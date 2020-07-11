46b0acf3354c22167a25a8926e42c0bf319d369f
Mateo balza <balzamateo5@gmail.com>
2020-07-11 17:01:04 -0500

    Se agregó pagina de diagramas

--- docs/main.js
+++ docs/main.js
@@ -11,3 +11,10 @@ $(document).ready(main);
            
         });
     }
+
+    $('.galeria_img').click(function(e){
+        var img = e.target.src;
+        var modal = `<div class="modal"><img src="${img}" alt="" class="modal_img"><div class="modal_boton">X</div></div>`
+            $('body').append(modal);
+
+    })
