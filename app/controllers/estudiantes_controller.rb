a9579ca84a37c135be81b1cd17bdc190ac054bb9
jhon rivera <jhonrivego@gmail.com>
2020-07-20 00:24:06 -0500

    se realizo vistas y controlador de los metodos new index

--- app/controllers/estudiantes_controller.rb
+++ app/controllers/estudiantes_controller.rb
@@ -1,13 +1,46 @@
 class EstudiantesController < ApplicationController
   def index
+    @estudiantes=Estudiante.all
   end
 
   def show
   end
 
   def new
+    @estudiante=Estudiante.new
+  end
+
+  def create
+    @estudiante = Estudiante.new(estudiante_params)
+    if @estudiante.save
+      redirect_to estudiantes_path
+    else
+      render :new
+    end
+    
+  end
+
+  def update
+    if @estudiante.update(estudiante_params)
+      redirect_to estudiantes_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @estudiante.destroy
+    redirect_to estudiantes_path
   end
 
   def edit
+  end
+
+  def set_estudiante
+    @estudiante = Estudiante.find(params[:id])
+  end
+
+  def estudiante_params
+    params.require(:estudiante).permit(:nombre,:apellido,:telefono,:direccion,:correo)
   end
 end
